0a23ee7694ce9feef664de682e569262a6f8e043
Udhaykrishn <krishnauday320@gmail.com>
2024-11-07 12:51:04 +0530

    created all auth router endpoints in router folder

---
+++ src/routes/auth.router.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  getLogin,
+  home,
+  postLogin,
+  postLogout,
+  getIndex,
+  PageNotFound,
+  getContact
+} from "../services/auth.services.js";
+import { isAuthenticted } from "../middleware/auth.middleware.js";
+
+const router = Router();
+router.get("/",isAuthenticted, getIndex);
+router.get("/login", getLogin);
+router.post("/login", postLogin);
+router.get("/home", isAuthenticted, home);
+router.post("/logout", postLogout);
+router.get('/contact',getContact)
+router.get("*", isAuthenticted, PageNotFound);
+
+export default router;
